595a94515470afa77be6b199bfc151ff02cf64cd
lee <248197941@qq.com>
2023-07-14 09:31:58 +0800

    v1.2.5

--- src/package/form-render4-vue3-pro/index.js
+++ src/package/form-render4-vue3-pro/index.js
@@ -1,9 +1,10 @@
 import FormRender4Vue3 from './src/form-render4-vue3-pro.vue'
 
+
 FormRender4Vue3.install = function (app) {
   app.component('form-render4-vue3-pro', FormRender4Vue3)
 }
 
-FormRender4Vue3.version = 'v1.2.4'
+FormRender4Vue3.version = 'v1.2.5'
 export default FormRender4Vue3
 
