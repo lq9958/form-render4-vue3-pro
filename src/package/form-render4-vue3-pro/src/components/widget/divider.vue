47f0d1e5e8cc47611dbbf9930e4629325c078291
lee <248197941@qq.com>
2024-03-15 10:55:04 +0800

    feat: 新增divider内置组件

---
+++ src/package/form-render4-vue3-pro/src/components/widget/divider.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-divider v-bind="attrs">{{ attrs.content }}</a-divider>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const props = defineProps({
+  schema: Object,
+})
+const schema = reactive(props.schema)
+
+const attrs = schema.props || {}
+</script>
+
+<script>
+export default {
+  name: 'FormRenderDivider',
+}
+</script>
